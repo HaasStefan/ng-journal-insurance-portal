507b40a911d07318f00ac06eb6d0fdf8befa2711
Stefan Haas <stefan.haas.privat@gmail.com>
2023-05-17 21:15:41 +0200

    feature generator

--- libs/plugins/ddd/src/generators/feature/generator.ts
+++ libs/plugins/ddd/src/generators/feature/generator.ts
@@ -1,20 +1,68 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
+import { formatFiles, getWorkspaceLayout, names, Tree } from '@nx/devkit';
 import { FeatureGeneratorSchema } from './schema';
+import { libraryGenerator } from '@nx/angular/generators';
+import { addFiles } from '../../lib/add-files';
+import { BaseNormaliedSchemaType } from '../../lib/normalized-schema';
+import { updateDomainDepConst } from '../../lib/update-domain-dep-const';
+
+type NormalizedSchema = FeatureGeneratorSchema &
+  BaseNormaliedSchemaType & {
+    componentName: string;
+  };
+
+function normalizeOptions(
+  tree: Tree,
+  options: FeatureGeneratorSchema
+): NormalizedSchema {
+  const name = `feature-${names(options.name).fileName}`;
+  const componentName = `${names(options.name).fileName}`;
+  const projectDirectory = options.domain
+    ? `${names(options.domain).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const domainDirectory = `${getWorkspaceLayout(tree).libsDir}/${
+    options.domain
+  }`;
+  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const parsedTags = [
+    'type:feature',
+    `domain:${names(options.domain).fileName}`,
+  ];
+
+  return {
+    ...options,
+    name,
+    componentName,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    domainDirectory,
+    parsedTags,
+  };
+}
 
 export default async function (tree: Tree, options: FeatureGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  const normalizedOptions = normalizeOptions(tree, options);
+
+  await libraryGenerator(tree, {
+    name: normalizedOptions.name,
+    directory: normalizedOptions.domainDirectory,
+    tags: normalizedOptions.parsedTags.join(','),
+    skipModule: true,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  addFiles(
+    tree,
+    {
+      ...normalizedOptions,
+      domainClassName: names(normalizedOptions.domain).className,
+      componentClassName: names(normalizedOptions.componentName).className,
+      domainFileName: names(options.domain).fileName,
+    },
+    __dirname
+  );
+
+  updateDomainDepConst(tree, normalizedOptions.domain);
+
   await formatFiles(tree);
 }
--- libs/plugins/ddd/src/generators/feature/schema.d.ts
+++ libs/plugins/ddd/src/generators/feature/schema.d.ts
@@ -1,3 +1,4 @@
 export interface FeatureGeneratorSchema {
   name: string;
+  domain: string;
 }
