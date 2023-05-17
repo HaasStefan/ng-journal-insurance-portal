9429f3d028cf47fffbfa1a62ec25a3cca970a823
Stefan Haas <stefan.haas.privat@gmail.com>
2023-05-17 23:21:55 +0200

    utils generator

--- libs/plugins/ddd/src/generators/utils/generator.ts
+++ libs/plugins/ddd/src/generators/utils/generator.ts
@@ -1,20 +1,58 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import * as path from 'path';
+import { formatFiles, getWorkspaceLayout, names, Tree } from '@nx/devkit';
 import { UtilsGeneratorSchema } from './schema';
+import { libraryGenerator } from '@nx/angular/generators';
+import { addFiles } from '../../lib/add-files';
+import { BaseNormaliedSchemaType } from '../../lib/normalized-schema';
+import { updateDomainDepConst } from '../../lib/update-domain-dep-const';
+
+type NormalizedSchema = UtilsGeneratorSchema & BaseNormaliedSchemaType;
+
+function normalizeOptions(
+  tree: Tree,
+  options: UtilsGeneratorSchema
+): NormalizedSchema {
+  const fileName = names(options.name).fileName;
+  const name =
+    fileName === 'utils'
+      ? fileName
+      : `utils-${names(options.name).fileName}`;
+  const projectDirectory = options.domain
+    ? `${names(options.domain).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const domainDirectory = `${getWorkspaceLayout(tree).libsDir}/${
+    options.domain
+  }`;
+  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const parsedTags = [
+    'type:utils',
+    `domain:${names(options.domain).fileName}`,
+  ];
+
+  return {
+    ...options,
+    name,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    domainDirectory,
+    parsedTags,
+  };
+}
 
 export default async function (tree: Tree, options: UtilsGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+  const normalizedOptions = normalizeOptions(tree, options);
+
+  await libraryGenerator(tree, {
+    name: normalizedOptions.name,
+    directory: normalizedOptions.domainDirectory,
+    tags: normalizedOptions.parsedTags.join(','),
+    skipModule: true,
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  addFiles(tree, normalizedOptions, __dirname);
+
+  updateDomainDepConst(tree, normalizedOptions.domain);
+
   await formatFiles(tree);
 }
--- libs/plugins/ddd/src/generators/utils/schema.d.ts
+++ libs/plugins/ddd/src/generators/utils/schema.d.ts
@@ -1,3 +1,4 @@
 export interface UtilsGeneratorSchema {
   name: string;
+  domain: string;
 }
