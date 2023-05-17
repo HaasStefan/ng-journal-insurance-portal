c2725b11630a9841bd5eeb478a810a4132577d6c
Stefan Haas <stefan.haas.privat@gmail.com>
2023-05-18 00:53:17 +0200

    stub api

--- libs/shared/utils/src/lib/injection-tokens.ts
+++ libs/shared/utils/src/lib/injection-tokens.ts
@@ -1,7 +1,6 @@
 import { InjectionToken } from "@angular/core";
 
-// todo: create real api
 export const BaseUrl = new InjectionToken<string>('BaseUrl', {
     providedIn: 'root',
-    factory: () => 'http://localhost:3333/api'
+    factory: () => location.origin
 });
